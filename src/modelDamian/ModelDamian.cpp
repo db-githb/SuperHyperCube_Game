8af5f549361a6f49bfe4d82499c7731ce8b9880d
D.B <snotboogieisdead@gmail.com>
2021-07-10 21:15:30 -0400

    finished Damian's Model

--- src/modelDamian/ModelDamian.cpp
+++ src/modelDamian/ModelDamian.cpp
@@ -3,6 +3,31 @@
 ModelDamian::ModelDamian() {
 	unitCube = UnitCube();
 	damiansShader = Shader("res/shaders/modelDamian.vert", "res/shaders/modelDamian.frag");
+
+	for (int r = 0; r < ROWS; r++) {
+		for (int c = 0; c < COLUMNS; c++) {
+			modelData[r][c] = WALL;
+		}
+	}
+
+	for (int r = 1; r < 8; r++) {
+		modelData[r][3] = BLUE;
+	}
+
+	for (int c = 1; c < 6; c++) {
+		
+		modelData[2][c] = RED;
+
+		if (c != 3) {
+			modelData[6][c] = RED;
+		}
+		
+		if (c == 2 || c == 4) {
+			modelData[4][c] = RED;
+		}
+	}
+
+
 }
 
 void ModelDamian::draw(Camera inCam, glm::vec3* dirLight, glm::mat4 projection, glm::mat4 view, glm::mat4 model) {
@@ -19,10 +44,32 @@ void ModelDamian::draw(Camera inCam, glm::vec3* dirLight, glm::mat4 projection, 
 	damiansShader.setMat4("projection", projection);
 	damiansShader.setMat4("view", view);
 
-	for (int i = 0; i < 1; i++) {
-		model = glm::translate(model, glm::vec3(1.0f, 0.5f, 2.0f));
-		damiansShader.setMat4("model", model);
-		glBindVertexArray(unitCube.getVAO());
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+	for (int r = 0; r < ROWS; r++) {
+		for (int c = 0; c < COLUMNS; c++) {
+			float x = (float)c;
+			float y = (float)r;
+			
+			model = glm::mat4(1.0f);
+
+			if (modelData[r][c] == WALL) {
+				damiansShader.setVec3("dirLight.ambient", dirLight[LIGHT_AMBIENT]);
+				model = glm::translate(model, glm::vec3(x + 0.5, y + 0.5f, -2.0f));
+			}
+			else {
+				
+				model = glm::translate(model, glm::vec3(x + 0.5, y + 0.5f, 2.0f));
+
+				if (modelData[r][c] == RED) {
+					damiansShader.setVec3("dirLight.ambient", glm::vec3(1.0f, 0.0f, 0.0f));
+				}
+				else {
+					damiansShader.setVec3("dirLight.ambient", glm::vec3(0.0f, 0.0f, 1.0f));
+				}
+			}
+
+			damiansShader.setMat4("model", model);
+			glBindVertexArray(unitCube.getVAO());
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 	}
 }
--- src/modelDamian/ModelDamian.h
+++ src/modelDamian/ModelDamian.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "../unitCube/UnitCube.h"
 
+#define ROWS 9
+#define COLUMNS 7
+
+#define WALL 0
+#define BLUE 1
+#define RED 2
+
 class ModelDamian {
+
 public:
 	ModelDamian();
 	void draw(Camera inCam, glm::vec3* dirLight, glm::mat4 projection, glm::mat4 view, glm::mat4 model);
@@ -9,4 +17,5 @@ public:
 private:
 	UnitCube unitCube;
 	Shader damiansShader;
+	int modelData[ROWS][COLUMNS];
 };
