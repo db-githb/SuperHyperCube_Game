dc0c85ab9856febae1fdee5cf2dc25c2bae3a95d
Kayla Charky <Kaylacharky@hotmail.com>
2021-07-16 19:01:37 -0400

    Randomize

--- src/modelKayla/ModelKayla.h
+++ src/modelKayla/ModelKayla.h
@@ -3,7 +3,7 @@
 
 #define ROWS 9
 #define COLUMNS 7
-#define PLANES 4
+#define PLANES 7
 
 
 #define WALL 0
@@ -15,7 +15,16 @@ class ModelKayla : public ModelBase {
 
 public:
 	void initialize();
+	void generateOriginalObject();
+	int* generateRandomModel(int x, int y, int z);
+	void resetUnitCubes();
+	void resetModel();
+
+	void generateCube(int rowStart, int rowEnd, int columnStart, int columnEnd, int planeStart, int planeEnd, int color);
+	
 	void draw(Camera inCam, glm::vec3* dirLight, glm::mat4 projection, glm::mat4 view, glm::mat4 model);
+
+	void positionModel(float x, float y, float z);
 
 private:
 	int modelData[ROWS][COLUMNS][PLANES];
