9f781f60b6f0f144fe685596bcb94a965c398b5f
D.B <snotboogieisdead@gmail.com>
2021-07-29 01:39:52 -0400

    tiled texture

--- src/gridLines/GridLines.cpp
+++ src/gridLines/GridLines.cpp
@@ -4,7 +4,7 @@ GridLines::GridLines() {
 	UnitCube unitCube= UnitCube();
 	gridLinesShader =  Shader("res/shaders/cubeShader.vert", "res/shaders/cubeShader.frag");
 
-	diffuseMap = gridLinesShader.loadTexture("res/images/tile.jpg");
+	diffuseMap = gridLinesShader.loadTexture("res/images/tile2.png");
 
 	// shader configuration
 	gridLinesShader.use();
@@ -29,48 +29,15 @@ void GridLines::draw(Camera inCam, glm::mat4 projection, glm::mat4 view, glm::ma
 
 	gridLinesShader.setVec3("viewPos", inCam.Position);
 	
-
-	for (double z = -50; z < 51; z++) {
-		model = glm::mat4(1.0f);
-		if ((int)z % 5 == 0) {
-			gridLinesShader.setVec3("Color", glm::vec3(.5, 0, .5));
-		}
-		else {
-			gridLinesShader.setVec3("Color", glm::vec3(0.0, 0.5, 0.0));
-		}
-		model = glm::translate(model, glm::vec3(0.0, 0.0, z));
-		model = glm::scale(model, glm::vec3(1.0, 1.0, 100.0));
-		gridLinesShader.setMat4("model", model);
-
-		// bind texture maps
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuseMap);
-
-		glBindVertexArray(unitCube.getVAO());
-
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-	}
-
-	// lines perpendicular to z-axis
-	for (double x = -50; x < 51; x++) {
-		model = glm::mat4(1.0f);
-		if ((int)x % 5 == 0) {
-			gridLinesShader.setVec3("Color", glm::vec3(.5, 0, .5));
-		}
-		else {
-			gridLinesShader.setVec3("Color", glm::vec3(0.0, 0.5, 0.0));
-		}
-
-		model = glm::translate(model, glm::vec3(x, 0.0, 0.0));
-		model = glm::scale(model, glm::vec3(1.0, 1.0, 100.0));
-		gridLinesShader.setMat4("model", model);
-
-		// bind texture maps
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuseMap);
-
-		glBindVertexArray(unitCube.getVAO());
-
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-	}
+	model = glm::scale(model, glm::vec3(100.0, 0.01, 100.0));
+	gridLinesShader.setMat4("model", model);
+	
+	// bind texture maps
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, diffuseMap);
+	
+	glBindVertexArray(unitCube.getVAO());
+	
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	
 }
