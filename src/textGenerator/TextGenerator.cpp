b6fcdaf18a2087b92d52a00f55e7c251c105654c
D.B <snotboogieisdead@gmail.com>
2021-08-21 14:27:59 -0400

    added reference to text generator

--- src/textGenerator/TextGenerator.cpp
+++ src/textGenerator/TextGenerator.cpp
@@ -1,3 +1,10 @@
+/*
+ * Text Generator class is adapted the learnOpenGL.com code repostitory on github: https://github.com/JoeyDeVries/LearnOpenGL
+   License:
+ * All code samples, unless explicitly stated otherwise, are licensed under the terms of the CC BY-NC 4.0 license as published by Creative Commons, either version 4 of the License, or (at your option) any later version.
+ * Link to license:  https://github.com/JoeyDeVries/LearnOpenGL/blob/master/LICENSE.md
+*/
+
 #include "TextGenerator.h"
 
 void TextGenerator::setup() {
